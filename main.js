bbedef78c32dcba6cfe94301f7198b97e3d71239
Cris <crisnguyen2923@hotmail.com>
2023-10-22 11:47:59 +0700

    Fix some bugs

---
+++ main.js
@@ -0,0 +1,56 @@
+const buyBtns = document.querySelectorAll(".js-buy-ticket");
+const modal = document.querySelector(".js-modal");
+const modalContainer = document.querySelector(".js-modal-container");
+const modalClose = document.querySelector(".js-modal-close");
+
+//Loops btn when click
+for (const buyBtn of buyBtns) {
+  buyBtn.onclick = function () {
+    modal.classList.add("open");
+  };
+}
+
+//turn off modal when clicking on the modal close button
+modalClose.onclick = function () {
+  modal.classList.remove("open");
+};
+
+modal.onclick = function () {
+  modal.classList.remove("open");
+};
+
+// Prevent when click on modal tag
+modalContainer.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
+
+//RESONSIVE MOBILE
+const header = document.getElementById("header");
+const mobileMenu = document.getElementById("mobile-icon");
+const headerHeight = header.clientHeight; //clientHeight: height of header
+
+//Open/close menu
+mobileMenu.onclick = function () {
+  const isClose = header.clientHeight === headerHeight; //neu bang thi la dang dong
+  if (isClose) {
+    header.style.height = "auto";
+  } else {
+    header.style.height = null;
+  }
+};
+
+//Auto close when click menu
+const menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+for (let i = 0; i < menuItems.length; i++) {
+  const menuItem = menuItems[i];
+
+  menuItem.onclick = function (event) {
+    const isParrentMenu = this.classList.contains("menu-more");
+    if (isParrentMenu) {
+      event.preventDefault();
+    } else {
+      header.style.height = null;
+    }
+  };
+}
